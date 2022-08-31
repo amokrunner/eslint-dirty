2a038f3ffbe5c82c0210bc11bc10d78b2da9b45e
amokrunner <danny@amokrunner.com>
2022-08-30 20:40:59 -0700

    cleanup

--- index.js
+++ index.js
@@ -18,14 +18,17 @@ function lintDirty (opts = {}) {
   console.log(`Running eslint-dirty${Object.keys(opts).length ? ' with options:' : '.'}`)
   Object.keys(opts).forEach(k => { console.log(`  - ${k}: ${opts[k]}`) })
   result('git diff --name-only HEAD | xargs', async (res, stdout, stderr) => {
-    const eslint = new ESLint(opts)
-
-    // 2. Lint files.
+    // turn git diff output into array:
     let arr = stdout.replace(/\n/g, '').split(' ')
-    arr = arr.filter(f => f.includes('.'))
-    arr = arr.filter(f => !f.includes('.json'))
-    arr = arr.filter(f => fs.existsSync(f))
 
+    // filter for valid files:
+    arr = arr.filter(f =>
+      f.includes('.') && // has an extension:
+      !f.includes('.json') && // is not json
+      fs.existsSync(f) // exists
+    )
+
+    // report files:
     if (arr.length) {
       console.log('Dirty files:')
       arr.forEach(f => { console.log(`  - ${f}`) })
@@ -34,17 +37,20 @@ function lintDirty (opts = {}) {
       return
     }
 
+    // run eslint:
+    const eslint = new ESLint(opts)
     const results = await eslint.lintFiles(arr)
 
+    // fix, if fixing:
     if (opts.fix) {
       await ESLint.outputFixes(results)
     }
 
-    // 3. Format the results.
+    // Format the results:
     const formatter = await eslint.loadFormatter('stylish')
     const resultText = formatter.format(results)
 
-    // 4. Output it.
+    // Output results
     console.log(resultText)
   })
 }
